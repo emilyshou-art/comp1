4ce0e088129e1e245e93e6eb9a441a8ce9c0f50a
emilyshou-art <emilyshou@alunos.utfpr.edu.br>
2025-10-08 09:20:57 -0300

    Add files via upload

---
+++ FunPassRefex3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void converteHora(int total_segundos, int *hora, int *min, int *seg);
+
+int main() {
+    int total, h, m, s;
+
+    printf("Digite a quantidade total de segundos: ");
+    scanf("%d", &total);
+
+    converteHora(total, &h, &m, &s);
+
+    printf("%02d:%02d:%02d\n", h, m, s);
+
+    return 0;
+}
+
+void converteHora(int total_segundos, int *hora, int *min, int *seg) {
+    *hora = total_segundos / 3600;
+    *min = (total_segundos % 3600) / 60;
+    *seg = total_segundos % 60;
+}
